68f4fd9aa8f5bfaa85da78b6753dc82589b2e5d3
cylia boukhiba <cylia.silouh@gmail.com>
2019-09-23 12:27:02 +0200

    mod

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+const  port = process.env.PORT ||  3000
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}!')
+})
